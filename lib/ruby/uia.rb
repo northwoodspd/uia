f878a44b620d848b43d906d8e230cd5ccee2a1b7
Levi Wilson <levi@leviwilson.com>
2013-10-11 03:09:07 -0400

    initial commit with loading the uia dll

--- lib/ruby/uia.rb
+++ lib/ruby/uia.rb
@@ -1,7 +1,41 @@
 require "ruby/uia/version"
+require 'ffi'
 
 module Ruby
   module Uia
-    # Your code goes here...
+    extend FFI::Library
+
+    class ElementStruct < FFI::ManagedStruct
+      layout :name, :string
+
+      def self.release(pointer)
+        Uia.release_element(pointer)
+      end
+    end
+
+    def self.uia_directory
+      File.dirname(__FILE__) + '/../../ext/UiaDll/Release'
+    end
+
+    ffi_lib File.join(uia_directory, 'UiaDll.dll')
+    ffi_convention :stdcall
+
+    attach_function :init, :initialize, [:string], :void
+    init(uia_directory)
+
+    attach_function :release_element, :Element_Release, [:pointer], :void
+    attach_function :Element_FindById, [:string, :pointer, :int], ElementStruct.by_ref
+
+    def self.find_by_id(id)
+      can_throw(:Element_FindById, id)
+    end
+
+    def self.can_throw(method, *args)
+      string_buffer = FFI::MemoryPointer.new :char, 1024
+      result = send method, *(args << string_buffer << 1024)
+      error_info = string_buffer.read_string
+      raise error_info unless error_info.empty?
+      result
+    end
   end
 end
--- ruby-uia.gemspec
+++ ruby-uia.gemspec
@@ -8,15 +8,17 @@ Gem::Specification.new do |spec|
   spec.version       = Ruby::Uia::VERSION
   spec.authors       = ["Levi Wilson"]
   spec.email         = ["levi@leviwilson.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
+  spec.description   = %q{This wraps UIA stuff}
+  spec.summary       = %q{This wraps UIA stuff}
   spec.homepage      = ""
   spec.license       = "MIT"
 
-  spec.files         = `git ls-files`.split($/)
+  spec.files         = `git ls-files`.split($/) + ['ext/UiaDll/Release/UiaDll.dll']
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
+
+  spec.add_runtime_dependency 'ffi'
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
